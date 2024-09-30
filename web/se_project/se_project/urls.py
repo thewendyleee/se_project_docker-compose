1454964175280d5bcb1a5e83899a8977dafa0868
thewendyleee <wendys94088@gmail.com>
2024-09-30 14:10:25 +0800

    upload

---
+++ web/se_project/se_project/urls.py
@@ -0,0 +1,41 @@
+"""se_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from car_rent.views import *
+
+urlpatterns = [
+    path('', login),
+    path('admin/', admin.site.urls),
+    path('rent/', rent),
+    path('report/', report),
+    path('transaction/',TransactionManager),
+    path('transaction_detail/<str:trans_id>/',TransDetailManager),
+    path('order/',OrderManager),
+    path('return_car/',return_car),
+    path('personal_info/',UserManager),
+    path('personal_info_update/',UserUpdateManager),
+    path('login/',login),
+    path('register/',register),
+    path('logout/', logout),
+
+    path('upload', UserUploadManager),
+    path('register_check/', register_check),
+    path('order_upload',order_upload),
+    path('finishrent/<str:Place>/<str:CarT>/',finishrent),
+    path('rent_car_num_check/', rent_car_num_check),
+
+]
